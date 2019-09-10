5288df88f29a6426b6b59f0ede4cdb40b85e7365
Minibrams <anders@brams.dk>
2019-09-10 23:48:54 +0200

    Corrected dbscripts for all services

--- AccountService/dbscripts/seed.sql
+++ AccountService/dbscripts/seed.sql
@@ -6,9 +6,9 @@ CREATE TABLE people (
   other_name  TEXT
 ) WITH (OIDS=FALSE);
 
-ALTER TABLE app OWNER TO ya;
+ALTER TABLE people OWNER TO ya;
 
-INSERT INTO app(name, other_name) VALUES(
+INSERT INTO people(name, other_name) VALUES(
   'Anders Brams',
   'The literal god'
 );

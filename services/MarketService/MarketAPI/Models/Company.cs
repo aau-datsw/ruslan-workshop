5844311ee7596cbb10c47c73e730f4b537442623
Minibrams <anders@brams.dk>
2019-09-16 23:16:47 +0200

    Making progress towards code-first database migrations

--- services/MarketService/MarketAPI/Models/Company.cs
+++ services/MarketService/MarketAPI/Models/Company.cs
@@ -1,3 +1,5 @@
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using Newtonsoft.Json;
 
 namespace MarketAPI.Models
@@ -13,6 +15,14 @@ namespace MarketAPI.Models
     [JsonProperty("volatility")]
     public int Volatility { get; set; }
 
+    public ICollection<Record> History { get; set; }
+
     public override string ToString() => $"{Id}: {Name}@{Price}DKK ({(Volatility == 0 ? "SLOW" : Volatility == 1 ? "NORMAL" : "VOLATILE")})";
+  }
+
+  public partial class Record
+  {
+    public int Timestamp { get; set; }
+    public int Price { get; set; }
   }
 }
--- services/MarketService/MarketAPI/Models/MarketAPIContext.cs
+++ services/MarketService/MarketAPI/Models/MarketAPIContext.cs
@@ -1,30 +1,11 @@
 using Microsoft.EntityFrameworkCore;
-using Microsoft.EntityFrameworkCore.Metadata.Builders;
 
 namespace MarketAPI.Models
 {
-  public class MarketAPIContext : DbContext
-  {
-    public DbSet<Company> Companies { get; set; }
-
-    public MarketAPIContext(DbContextOptions<MarketAPIContext> options) 
-    : base(options) { }
-
-    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    public class MarketAPIContext : DbContext
     {
-        base.OnModelCreating(modelBuilder);        
-        MapCompanies(modelBuilder.Entity<Company>());
-    }
+        public MarketAPIContext(DbContextOptions<MarketAPIContext> options) : base(options) { } 
 
-    private void MapCompanies(EntityTypeBuilder<Company> entityBuilder)
-    {
-        entityBuilder.HasKey(x => x.Id);
-        entityBuilder.ToTable("company");
-
-        entityBuilder.Property(x => x.Id).HasColumnName("id");
-        entityBuilder.Property(x => x.Name).HasColumnName("name");
-        entityBuilder.Property(x => x.Price).HasColumnName("price");
-        entityBuilder.Property(x => x.Volatility).HasColumnName("volatility");
+        public DbSet<Company> Companies { get; set; }
     }
-  }
 }

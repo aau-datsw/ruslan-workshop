270f72e3c649e0601b24b58a052e481a1997a673
Minibrams <anders@brams.dk>
2019-10-06 14:04:42 +0200

    Normalised market.json to market_norm.json

--- data/gen_companies.py
+++ data/gen_companies.py
@@ -1,49 +1,19 @@
 import json
-from random import random
-
-CONFIG = {
-    'volatile' : {
-        'price_seed' : 2000,
-        'change_rate' : 1000
-    },
-    'normal' : {
-        'price_seed' : 10000,
-        'change_rate' : 2000,
-    },
-    'slow' : {
-        'price_seed' : 50000,
-        'change_rate' : 5000
-    }
-}
-
 
 if __name__ == "__main__": 
-    companies = []
-    company_names = None 
-    with open('company_names.json') as fp: 
-        company_names = [o['name'] for o in json.load(fp)]
-    
-    print(f'Loaded {len(company_names)} company names.')
-    group_size = int(len(company_names) * 1/3)
+    with open("market.json") as fp: 
+        js = json.load(fp)
+        min_price = min([o["y"] for o in js["data"]])
 
-    volatile_companies = company_names[:group_size]
-    slow_companies = company_names[group_size:group_size * 2]
-    normal_companies = company_names[group_size * 2:]
+        with open("market_norm.json", "w+") as new_fp:
+            json.dump({
+                "name" : "Ligma Inc.", 
+                "data" : [
+                    {
+                        "x" : i,
+                        "y" : o["y"] + abs(min_price) + 1
+                    }
 
-    for volatility, company_group in {
-        'volatile' : volatile_companies,
-        'normal' : normal_companies,
-        'slow' : slow_companies
-    }.items(): 
-        for name in company_group: 
-            price_seed = CONFIG[volatility]['price_seed']
-            variance = random() * CONFIG[volatility]['change_rate']
-            price = price_seed + variance if random() > 0.5 else price_seed - variance
-            companies.append({
-                'name' : name,
-                'price' : int(price),
-                'volatility' : ['slow', 'normal', 'volatile'].index(volatility)
-            })
-
-    with open('companies.json', 'w+') as fp: 
-        json.dump(companies, fp, indent=4, sort_keys=True)
+                    for i, o in enumerate(js["data"])
+                ]
+            }, new_fp, indent=True)

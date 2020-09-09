48aa30c88cae89a55b1e630bab6a5f4df84e8a44
MrPekar98 <martin.pekar@outlook.dk>
2020-09-09 22:07:43 +0200

    Added variable to tell size of market data array.

--- CRuslan/framework/transmitter.h
+++ CRuslan/framework/transmitter.h
@@ -10,6 +10,10 @@ struct group
     unsigned balance, stock_count, stock_value, total_value;
 };
 
+// Size of market data array.
+volatile unsigned MARKET_DATA_SZ = 0
+
+// Prototypes.
 int *get_market_data(struct tm from, struct tm to);
 struct group get_info();
 void buy();
